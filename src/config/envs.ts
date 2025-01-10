ef5a1db6089b313967f4d2e05689439c98f908f0
red <redmoart@gmail.com>
2025-01-10 14:45:45 -0500

    envs configuration ⚙

---
+++ src/config/envs.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  MONGODB_DATABASE_URL: string;
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
+  AWS_REGION: string;
+  AWS_S3_BUCKET_NAME: string;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    MONGODB_DATABASE_URL: joi.string().required(),
+    AWS_ACCESS_KEY_ID: joi.string().required(),
+    AWS_SECRET_ACCESS_KEY: joi.string().required(),
+    AWS_REGION: joi.string().required(),
+    AWS_S3_BUCKET_NAME: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: \${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  mongodbDatabaseUrl: envVars.MONGODB_DATABASE_URL,
+  awsAccessKeyId: envVars.AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: envVars.AWS_SECRET_ACCESS_KEY,
+  awsRegion: envVars.AWS_REGION,
+  awsS3BucketName: envVars.AWS_S3_BUCKET_NAME,
+};
