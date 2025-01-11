febfc16a64c5f3474f5192383766ae01b5a286a0
redmoart23 <108553638+redmoart23@users.noreply.github.com>
2025-01-10 22:29:44 -0500

    feat: :sparkles: implement image upload functionality with AWS S3 integration ✨ (#3)

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,29 +1,89 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { CreateImageDto } from './dto/create-image.dto';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+
+import * as sharp from 'sharp';
 import { PrismaClient } from '@prisma/client';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+import { UploadImageDto } from './dto/upload-image.dto';
+import { envs } from 'src/config/envs';
 
 @Injectable()
 export class ImagesService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('ImagesService');
 
+  // Aws S3 client config
+  private readonly s3 = new S3Client({
+    region: envs.awsRegion,
+    credentials: {
+      accessKeyId: envs.awsAccessKeyId,
+      secretAccessKey: envs.awsSecretAccessKey,
+    },
+  });
+
+  // Database connection
   async onModuleInit() {
     await this.$connect();
     this.logger.log('MongoDB connected successfully');
   }
 
-  create(createImageDto: CreateImageDto) {
-    return 'This action adds a new image' + createImageDto;
+  async uploadImage(uploadImageDto: UploadImageDto, file: Express.Multer.File) {
+    const fileKey = `${Date.now()}.png`;
+    const pngBuffer = await sharp(file.buffer).png().toBuffer();
+
+    try {
+      await this.s3.send(
+        new PutObjectCommand({
+          Bucket: envs.awsS3BucketName,
+          Key: fileKey,
+          Body: pngBuffer,
+          ContentType: 'image/png',
+          ACL: 'public-read',
+        }),
+      );
+
+      const imageUrl = `https://${envs.awsS3BucketName}.s3.${envs.awsRegion}.amazonaws.com/${fileKey}`;
+
+      const image = await this.image.create({
+        data: {
+          uploadedBy: uploadImageDto.uploadedBy,
+          imageUrl: imageUrl,
+        },
+      });
+      return image;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Unable to upload image, please try again',
+        error,
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all images`;
+  async findAll() {
+    return await this.image.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} image`;
+  async findOne(id: string) {
+    try {
+      return await this.image.findUnique({ where: { id: id } });
+    } catch (error) {
+      throw new NotFoundException('Image not found or invalid id', error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} image`;
+  async remove(id: string) {
+    try {
+      return await this.image.delete({ where: { id: id } });
+    } catch (error) {
+      throw new NotFoundException(
+        'Unable to delete, Image not found or invalid id',
+        error,
+      );
+    }
   }
 }
